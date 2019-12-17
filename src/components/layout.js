9f67f69c0b6985ea692d89c52064385f4914c670
jack gaarkeuken <jack.gaarkeuken@gmail.com>
2019-12-17 15:32:31 -0500

    layout-identity context added

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "gatsby";
-import { IdentityContextProvider } from "react-netlify-indentity-widget";
+import { IdentityContextProvider } from "react-netlify-identity-widget";
 
 import "./layout.css";
 
 const Layout = ({ children }) => (
-  <IndentityContextProvider url="https://fem-jamstack-intro-auth.netlify.com">
+  <IdentityContextProvider url="https://jamstack-intro-auth.netlify.com">
     <header>
-      <Link to="/">JAMStack App</Link>
+      <Link to="/">JAMstack App</Link>
     </header>
     <main>{children}</main>
-  <IndentityContextProvider/>
+  </IdentityContextProvider>
 );
 
 export default Layout;
